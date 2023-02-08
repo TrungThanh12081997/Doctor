b4c8bbd54ec20c69a15509496889194556afaa38
trungthanh1208 <thanh.qt@novaeforce.vn>
2023-02-08 16:34:02 +0700

    update db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,3 +6,5 @@ let getAboutPage = (req, res) => {
 }
 
 module.exports = { getHomePage, getAboutPage }
+// Controller
+// Giữ nhiệm vụ tiếp nhận điều hướng các yêu cầu từ người dùng, giúp lấy dữ liệu đúng với những thông tin cần thiết nhờ vào các nghiệp vụ lớp Model cung cấp và hiển thị các dữ liệu đó ra cho người dùng.
